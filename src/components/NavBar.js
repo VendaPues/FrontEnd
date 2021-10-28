5caca62e9bb6d51535bc596a0effd98ce84a4134
sf-burgos <brayan.burgosdelgado@kinesso.com>
2021-10-28 15:24:03 -0500

    create the components

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+
+
+export const Product = () => {
+
+    return (
+        <b>
+            Nav Bar
+        </b>
+
+    );
+
+}
---
+++ src/components/Product.js
@@ -0,0 +1,12 @@
+
+
+export const Product = () => {
+
+    return (
+        <b>
+            Hola parce
+        </b>
+
+    );
+
+}
